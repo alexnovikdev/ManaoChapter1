69e265974dcdf383c379c088436d4942c801a3ab
alexnovikdev <alexnovikdev@gmail.com>
2017-02-07 12:50:12 +0300

    upload file

---
+++ task1.php
@@ -0,0 +1,14 @@
+<?php
+
+$N = 1234567890;
+$count = 0;
+$stringN = (string)$N;
+
+$length = strlen((string)$N);
+for ($i = 0; $i < $length; $i++) {
+    if ((int)$stringN{$i} < 5) {
+        $count++;
+    }
+}
+
+echo $count;
