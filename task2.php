f8ea07f93ca63b4c06c249edc2a8d00b6bf299a5
alexnovikdev <alexnovikdev@gmail.com>
2017-02-07 17:49:53 +0300

    fix

--- task2.php
+++ task2.php
@@ -3,14 +3,13 @@
 $n = 15;
 
 for ($i = 1000; $i <= 9999; $i++) {
+    $sum = 0;
     $number = $i;
-    $arr = [];
     while ($number/10 > 0) {
-        $arr[] = $number % 10;
+        $sum += $number % 10;
         $number = (int)($number / 10);
     }
-    list($four, $three, $two, $one) = $arr;
-    if (($one + $two + $three + $four) == $n) {
-        echo implode(array_reverse($arr)) . "\n";
+    if ($sum == $n) {
+        echo $i . "\n";
     };
 }
