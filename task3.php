0bf7039f6f598190f528e9695cf0cfc3c9499cc7
alexnovikdev <alexnovikdev@gmail.com>
2017-02-08 14:04:48 +0300

    fix2

--- task3.php
+++ task3.php
@@ -2,12 +2,9 @@
 
 $N = 123456;
 $currentNumber = $N % 10;
+
 while ($N/10 > 0) {
-    $lastNumber = $N % 10;
-    if ($currentNumber == $lastNumber) {
-        $N = (int)($N / 10);
-        continue;
-    }
+    $lastNumber = (int)(($N % 100) / 10);
     if ($lastNumber < $currentNumber) {
         $currentNumber = $lastNumber;
         $N = (int)($N / 10);
