9b3defe5accc32e8c911e669d750d29ae6d6a9b2
alexnovikdev <alexnovikdev@gmail.com>
2017-02-08 14:26:07 +0300

    fix3

--- task3.php
+++ task3.php
@@ -3,16 +3,18 @@
 $N = 123456;
 $currentNumber = $N % 10;
 
-while ($N/10 > 0) {
-    $lastNumber = (int)(($N % 100) / 10);
+do {
+    $N = (int)($N / 10);
+    $lastNumber = $N % 10;
     if ($lastNumber < $currentNumber) {
         $currentNumber = $lastNumber;
-        $N = (int)($N / 10);
     } else {
         echo "Последовательность не возрастающая";
         break;
     }
-}
+
+} while ($N/10 > 0);
+
 if ($N == 0) {
     echo "Последовательность возрастаюшая";
 }
