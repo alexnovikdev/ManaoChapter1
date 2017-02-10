816e1cc54aa3d994802183fa862a63e5b253ed5b
alexnovikdev <alexnovikdev@gmail.com>
2017-02-10 12:36:30 +0300

    fix2

--- task4.php
+++ task4.php
@@ -7,36 +7,30 @@ for ($i = 1000; $i < 10000; $i += 2) {
     $number = $i;
     $currentNum = $number % 10;
 
-
     do {
-
         $number = (int)($number / 10);
         $lastNumber = $number % 10;
 
-        if ($lastNumber < $currentNum) {
-            $up = true;
-        } else {
-            $down = true;
-        }
-
-        if ($up xor $down) {
-
+        if (($up xor $down) || $i == 1000) {
             if ($lastNumber < $currentNum) {
                 $currentNum = $lastNumber;
                 $up = true;
-            } elseif ($lastNumber > $currentNum) {
+            }
+            if ($lastNumber > $currentNum) {
                 $currentNum = $lastNumber;
                 $down = true;
-            } else {
+            }
+            if ($lastNumber == $currentNum) {
                 break;
             }
         } else {
             break;
         }
 
-        if ((int)($number/10) == 0) {
-            echo $i . "\n";
-        }
-
     } while ((int)($number/10) != 0);
+
+    if ((int)($number/10) == 0) {
+        echo $i . "\n";
+    }
+
 }
