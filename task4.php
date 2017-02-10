1cf77ee0cfa4beedbc03f5faf3ecce236cf4c47a
alexnovikdev <alexnovikdev@gmail.com>
2017-02-10 15:53:56 +0300

    first number

--- task4.php
+++ task4.php
@@ -2,32 +2,46 @@
 
 for ($i = 1000; $i < 10000; $i += 2) {
 
-    $up = false;
-    $down = false;
     $number = $i;
     $currentNum = $number % 10;
+    $up = false;
+    $down = false;
+
+
+
+    if ((int)(($number % 100)/10) < $currentNum) {
+        $up = true;
+    } elseif ((int)(($number % 100)/10) > $currentNum) {
+        $down = true;
+    } else {
+        continue;
+    }
 
     do {
         $number = (int)($number / 10);
         $lastNumber = $number % 10;
 
-        if (($up xor $down) || $i == 1000) {
+        if ($up) {
             if ($lastNumber < $currentNum) {
                 $currentNum = $lastNumber;
-                $up = true;
-            }
-            if ($lastNumber > $currentNum) {
-                $currentNum = $lastNumber;
-                $down = true;
-            }
-            if ($lastNumber == $currentNum) {
+            } else {
                 break;
             }
-        } else {
-            break;
         }
+        if ($down) {
+            if ($lastNumber > $currentNum) {
+                $currentNum = $lastNumber;
+            } else {
+                break;
+            }
+        }
+        /*if ($lastNumber == $currentNum) {
+            echo "third\n";
+            break;
+        }*/
 
     } while ((int)($number/10) != 0);
+
 
     if ((int)($number/10) == 0) {
         echo $i . "\n";
