662b35d7f7facda262e50e6de50f38fb3263fbf3
alexnovikdev <alexnovikdev@gmail.com>
2017-02-13 17:03:46 +0300

    fix

--- task7.php
+++ task7.php
@@ -3,10 +3,9 @@
 /*Выяснить, есть ли в записи натурального числа N две одинаковые цифры.*/
 
 $N = 123456789;
-$count = 0;
-$outerCount = 0;
 
 while ($N > 0) {
+    $count = 0;
     $copyN = $N;
     $res = $N % 10;
     while ($copyN > 0) {
@@ -15,12 +14,11 @@ while ($N > 0) {
         }
         $copyN = (int)($copyN / 10);
     }
+    if ($count > 1) {
+        echo "одинаковые цифры есть";
+        exit;
+    }
     $N = (int)($N / 10);
-    $outerCount++;
 }
 
-if ($outerCount == $count) {
-    echo "одинаковых цифр нет";
-} else {
-    echo "одинаковые цифры есть";
-}
+echo "одинаковых цифр нет";
