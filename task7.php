27e7359af9b67849e926ad40a2430c318ce0fc63
alexnovikdev <alexnovikdev@gmail.com>
2017-02-14 12:09:19 +0300

    next fix

--- task7.php
+++ task7.php
@@ -7,19 +7,16 @@ $flag = false;
 
 while ($N > 0) {
     $count = 0;
-    $copyN = $N;
     $res = $N % 10;
+    $N = (int)($N / 10);
+    $copyN = $N;
     while ($copyN > 0) {
         if ($copyN % 10 == $res) {
-            $count++;
-        }
-        if ($count > 1) {
             $flag = true;
             break 2;
         }
         $copyN = (int)($copyN / 10);
     }
-    $N = (int)($N / 10);
 }
 
 if ($flag) {
