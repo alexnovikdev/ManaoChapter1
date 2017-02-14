f95d31494c0eb05cf70bf79e41b6fee3fe3b3cb1
alexnovikdev <alexnovikdev@gmail.com>
2017-02-14 11:34:02 +0300

    new rules

--- task6.php
+++ task6.php
@@ -1,6 +1,6 @@
 <?php
 
-/*Получить  все  четырехзначные  числа,  в  записи  которых  встречаются только цифры 0,2,3,7.*/
+/*Получить  все  четырехзначные  числа,  в  записи  которых  встречаются только цифры 0,2,3,7. UPD  и  И НЕ ПОВТОРЯЮТСЯ*/
 
 for ($i = 1000; $i < 10000; $i++) {
 
@@ -8,10 +8,40 @@ for ($i = 1000; $i < 10000; $i++) {
     $div = 10;
     $resultCount = 0;
 
+    $caseNull = 0;
+    $caseTwo = 0;
+    $caseThree = 0;
+    $caseSeven = 0;
+
     while ($number > 0) {
-        if (($number % $div) == 0 || ($number % $div) == 2 || ($number % $div) == 3 || ($number % $div) == 7) {
-            $resultCount++;
+
+        $num = $number % $div;
+
+        switch ($num) {
+            case 0:
+                $resultCount++;
+                $caseNull++;
+                break;
+            case 2:
+                $resultCount++;
+                $caseTwo++;
+                break;
+            case 3:
+                $resultCount++;
+                $caseThree++;
+                break;
+            case 7:
+                $resultCount++;
+                $caseSeven++;
+                break;
+            default:
+                continue 3;
         }
+
+        if ($caseNull > 1 || $caseTwo > 1 || $caseThree > 1 || $caseSeven > 1) {
+            continue 2;
+        }
+
         $number = (int)($number / $div);
     }
 
